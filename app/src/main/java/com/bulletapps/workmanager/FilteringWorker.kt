67094d61cdb132c2611da0ecaf8e60bec463a7ed
João Victor Sena <jvsena16@gmail.com>
2021-04-14 13:29:48 -0300

    Initial commit

---
+++ app/src/main/java/com/bulletapps/workmanager/FilteringWorker.kt
@@ -0,0 +1,26 @@
+package com.bulletapps.workmanager
+
+import android.content.ContentValues
+import android.content.Context
+import android.util.Log
+import androidx.work.Data
+import androidx.work.ListenableWorker
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
+
+class FilteringWorker(context: Context, params: WorkerParameters): Worker(context,params) {
+
+    override fun doWork(): ListenableWorker.Result {
+        try {
+            for (i in 0..600){
+                Log.i(ContentValues.TAG, "Filtering: $i")
+            }
+
+            return ListenableWorker.Result.success()
+        }catch (e:Exception){
+            return ListenableWorker.Result.failure()
+        }
+    }
+}
---
+++ app/src/main/java/com/bulletapps/workmanager/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.bulletapps.workmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val KEY_COUNT_VALUE = "key_count"
+    }
+
+    lateinit var textView:TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+
+        button.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest(){
+        val workManager = WorkManager.getInstance(applicationContext)
+
+        val data = Data.Builder()
+            .putInt(KEY_COUNT_VALUE,12)
+            .build()
+
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setInputData(data)
+            .setConstraints(constraints)
+            .build()
+
+        val filteringRequest = OneTimeWorkRequest.Builder(FilteringWorker::class.java)
+            .build()
+
+        val compressingRequest = OneTimeWorkRequest.Builder(CompressingWorker::class.java)
+            .build()
+
+        workManager
+            .beginWith(filteringRequest)
+            .then(compressingRequest)
+            .then(uploadRequest)
+            .enqueue()
+        
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this, {
+            textView.text = it.state.name
+
+            if (it.state.isFinished) {
+                val data = it.outputData
+                val message = data.getString(UploadWorker.KEY_WORKER)
+                Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+}
